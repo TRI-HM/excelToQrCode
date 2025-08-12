8118abb8fded93dba8ee11caa31bbe6c9e9b52ad
TriPCViviix <trihuynh@viviix.com>
2025-08-12 15:02:30 +0700

    apec_hn

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import qrcode
 import urllib.parse
 
 # Đường dẫn file Excel và thư mục xuất QR code
-input_file = "0723-Guest-data-ABB.xlsx"  # Đổi thành đường dẫn tới file Excel của bạn
+input_file = "apec_hn.xlsx"  # Đổi thành đường dẫn tới file Excel của bạn
 output_dir = "export"
 
 # Tạo thư mục export nếu chưa tồn tại
@@ -17,29 +17,24 @@ df = pd.read_excel(input_file)
 # Duyệt qua từng dòng trong DataFrame
 for index, row in df.iterrows():
     # Lấy giá trị id và name từ dòng hiện tại
-    user_id = row['ID']
-    name = row['Name']
-    title= row['Title'] if 'Title' in row else ''
+    user_id = row['no']
+    name = row['name']
+    link = row['link'] if 'link' in row else ''
 
     # Lấy giá trị dob từ dòng hiện tại và chuyển sang định dạng dd-mm-yyyy
     # dob = row['dob']
     # dob = str(dob).replace("/", "-")
 
     # Mã hóa name sang Base64
-    encoded_name = urllib.parse.quote(name)
-    decode_name = urllib.parse.unquote(encoded_name)
-
-    # Mã hoá title sang base64
-    encoded_title = urllib.parse.quote(title)
-    decode_title = urllib.parse.unquote(encoded_title)
+    # encoded_name = urllib.parse.quote(name)
+    # decode_name = urllib.parse.unquote(encoded_name)
     
     # Thay thế dấu "+" và "/" thành "-"
-    encoded_name = encoded_name.replace("+", "-").replace("/", "-").replace("=", "")
-    encoded_title = encoded_title.replace("+", "-").replace("/", "-").replace("=", "")
+    # encoded_name = encoded_name.replace("+", "-").replace("/", "-").replace("=", "")
 
     # Tạo giá trị cho QR code với name đã mã hóa
     # qr_value = f"https://huynhminhtri.dev?uuid={user_id}&name={encoded_name}&dob={dob}"
-    qr_value = f"id={user_id}&name={encoded_name}&title={encoded_title}"
+    # qr_value = f"id={user_id}&name={encoded_name}&title={encoded_title}"
     
     # Tạo QR code
     qr = qrcode.QRCode(
@@ -48,7 +43,7 @@ for index, row in df.iterrows():
         box_size=10,
         border=4,
     )
-    qr.add_data(qr_value)
+    qr.add_data(link)
     qr.make(fit=True)
     
     # Tạo ảnh QR code
@@ -57,5 +52,5 @@ for index, row in df.iterrows():
     # Đường dẫn lưu file QR code
     output_path = os.path.join(output_dir, f"{user_id}_{name}.png")
     img.save(output_path)
-    print(f"Đã tạo QR code cho {name} với id {user_id} và title {title}. Lưu tại {output_path}")
-    print(f"Giá trị QR code: {qr_value}")
+    print(f"Đã tạo QR code cho {name}. Lưu tại {output_path}")
+    print(f"Giá trị QR code: {link}")
